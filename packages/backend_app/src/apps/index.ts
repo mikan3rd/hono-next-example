4a086f8d02eb96fa1c991b9b6ecc187731a0c92b
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-03-13 14:06:13 +0900

    fix: Hono for RPC with cors

--- packages/backend_app/src/apps/index.ts
+++ packages/backend_app/src/apps/index.ts
@@ -3,8 +3,11 @@ import { swaggerUI } from "@hono/swagger-ui";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { cors } from "hono/cors";
 
-const app = new OpenAPIHono()
-  .use("*", cors())
+const app = new OpenAPIHono();
+
+app.use("*", cors());
+
+const routes = app
   .route("/hellos", helloApp)
   .doc("/doc", {
     openapi: "3.0.0",
@@ -16,4 +19,4 @@ const app = new OpenAPIHono()
   .get("/ui", swaggerUI({ url: "/doc" }));
 
 export { app };
-export type AppType = typeof app;
+export type AppType = typeof routes;
