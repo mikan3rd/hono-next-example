1b2aea5fd0cb79bf0d580e0aeaeb4a835cceebf6
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-03-19 00:33:35 +0900

    chore: helloページにメタデータ生成機能を追加し、APIからのメッセージをタイトルに反映

--- packages/frontend_app/src/app/hello/page.tsx
+++ packages/frontend_app/src/app/hello/page.tsx
@@ -3,8 +3,20 @@ import {
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
+import type { Metadata, ResolvingMetadata } from "next";
 import { getHello } from "../../components/pages/hello/client";
 import { Index } from "../../components/pages/hello/index";
+
+export async function generateMetadata(
+  _: unknown,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const { message } = await getHello();
+  const metadata = await parent;
+  return {
+    title: `message: ${message} | ${metadata.title?.absolute}`,
+  };
+}
 
 export default async function Hello() {
   const queryClient = new QueryClient();
