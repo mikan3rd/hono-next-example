45b25e42f2a0c393fe4a9600383ac47e6339e38c
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-03-24 17:04:46 +0900

    feat(baseClient): 環境に応じたbaseClientのURL設定を追加

--- packages/frontend_app/src/baseClient.ts
+++ packages/frontend_app/src/baseClient.ts
@@ -1,4 +1,10 @@
 import type { AppType } from "backend_app/src/apps";
 import { hc } from "hono/client";
 import { env } from "./env";
-export const baseClient = hc<AppType>(env.NEXT_PUBLIC_BACKEND_APP_URL);
+
+const baseUrl =
+  typeof window === "undefined"
+    ? env.BACKEND_APP_SERVER_URL
+    : env.NEXT_PUBLIC_BACKEND_APP_URL;
+
+export const baseClient = hc<AppType>(baseUrl);
