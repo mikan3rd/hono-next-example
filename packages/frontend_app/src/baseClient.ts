1d544970e3c8e21d9fe03c984f431ddcd2b2842a
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-03-24 17:08:51 +0900

    fix(baseClient): Docker Compose環境でのURL設定に関するFIXMEコメントを追加

--- packages/frontend_app/src/baseClient.ts
+++ packages/frontend_app/src/baseClient.ts
@@ -2,6 +2,12 @@ import type { AppType } from "backend_app/src/apps";
 import { hc } from "hono/client";
 import { env } from "./env";
 
+/**
+ * FIXME:
+ * When using docker compose, the URLs are different
+ * because the server-side request executed in the container
+ * and the client-side execution executed from the browser must be considered.
+ */
 const baseUrl =
   typeof window === "undefined"
     ? env.BACKEND_APP_SERVER_URL
