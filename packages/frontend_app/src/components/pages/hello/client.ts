28453b8519944cbc28908bb504f2d44f038244da
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-03-25 11:34:42 +0900

    feat(hello): エラーハンドリングを追加
    - `getHello`関数とボタンのクリックハンドラにおいて、レスポンスが正常でない場合にエラーをスローする処理を追加

--- packages/frontend_app/src/components/pages/hello/client.ts
+++ packages/frontend_app/src/components/pages/hello/client.ts
@@ -2,5 +2,8 @@ import { baseClient } from "../../../baseClient";
 
 export const getHello = async () => {
   const res = await baseClient.hellos.$get({ query: { name: "Hono" } });
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
   return res.json();
 };
