5e712589079084d789135330d00c80fc283efb88
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-03-30 19:40:03 +0900

    feat(posts): postsエンドポイントのOpenAPIスキーマを更新し、postオブジェクトを定義
    - OpenAPI仕様に基づき、postsエンドポイントのレスポンススキーマを修正
    - 新しいpostオブジェクトを定義し、必要なプロパティを追加
    - DTOスキーマを更新し、postオブジェクトをレスポンスに含めるように変更

--- packages/backend_app/src/apps/posts/dto.ts
+++ packages/backend_app/src/apps/posts/dto.ts
@@ -2,7 +2,7 @@ import { z } from "@hono/zod-openapi";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { postsTable } from "../../db/schema";
 
-const postSelectSchema = createSelectSchema(postsTable);
+const postSelectSchema = createSelectSchema(postsTable).openapi("post");
 const postInsertSchema = createInsertSchema(postsTable);
 
 export const getPostsResponseSchema = z.object({
@@ -13,4 +13,6 @@ export const postPostRequestSchema = postInsertSchema.pick({
   content: true,
 });
 
-export const postPostResponseSchema = postSelectSchema;
+export const postPostResponseSchema = z.object({
+  post: postSelectSchema,
+});
