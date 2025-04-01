f79d2924b876abc92e96ccddcade0e7e82032577
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-04-01 09:38:37 +0900

    feat(tests): getPostsRouteの200レスポンスを確認するテストケースを追加

--- packages/backend_app/src/apps/posts/index.test.ts
+++ packages/backend_app/src/apps/posts/index.test.ts
@@ -3,6 +3,13 @@ import { testClient } from "hono/testing";
 import { app } from "../../apps";
 
 describe("postsApp", () => {
+  describe("getPostsRoute", () => {
+    it("should return 200 Response", async () => {
+      const res = await testClient(app).posts.$get();
+      expect(res.status).toBe(200);
+    });
+  });
+
   describe("postPostRoute", () => {
     let content: string;
 
