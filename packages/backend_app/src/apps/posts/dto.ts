0a9288fe936a7158b3b7d6b394e90c1898bd0166
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-04-01 10:30:39 +0900

    feat(docker): Docker Composeに環境変数ファイルを追加し、db:pushコマンドを修正

--- packages/backend_app/src/apps/posts/dto.ts
+++ packages/backend_app/src/apps/posts/dto.ts
@@ -2,14 +2,29 @@ import { z } from "@hono/zod-openapi";
 import { postsTable } from "../../db/schema";
 import { createInsertSchema, createSelectSchema } from "../factory";
 
-const postSelectSchema = createSelectSchema(postsTable).openapi("post");
+const postSelectSchema = createSelectSchema(postsTable, {
+  id: (schema) =>
+    schema.openapi({
+      description: "Primary ID",
+      example: 1,
+    }),
+  content: (schema) =>
+    schema.openapi({
+      description: "The content of the post",
+      example: "test",
+    }),
+}).openapi("post");
 
 export const getPostsResponseSchema = z.object({
   posts: postSelectSchema.array(),
 });
 
 export const postPostRequestSchema = createInsertSchema(postsTable, {
-  content: (schema) => schema.min(1),
+  content: (schema) =>
+    schema.min(1).openapi({
+      description: "The content of the post",
+      example: "test",
+    }),
 }).pick({
   content: true,
 });
