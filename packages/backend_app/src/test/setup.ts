342fa9992211beda7967c70464b9e87c29641f59
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-04-01 09:36:56 +0900

    feat(tests): テスト実行前に全テーブルをトランケートする機能を追加

--- packages/backend_app/src/test/setup.ts
+++ packages/backend_app/src/test/setup.ts
@@ -1,5 +1,24 @@
 import { beforeEach } from "bun:test";
+import { sql } from "drizzle-orm";
+import { db } from "../db";
 
-beforeEach(() => {
-  // TODO: setup test database
+const truncateAllTables = async () => {
+  const tables = await db.execute(
+    sql`
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema = 'public';
+    `,
+  );
+  for (const table of tables.rows) {
+    const query = sql.raw(
+      `TRUNCATE TABLE ${table.table_name} RESTART IDENTITY CASCADE`,
+    );
+    await db.execute(query);
+  }
+  console.info("All tables truncated");
+};
+
+beforeEach(async () => {
+  await truncateAllTables();
 });
