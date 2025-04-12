3b48037c55cd08311d1cf7797fabe39b2b974874
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-04-12 23:25:02 +0900

    feat(api): エラーレスポンスにコードを追加し、スキーマを改善

--- packages/backend_app/src/dto/output/error.ts
+++ packages/backend_app/src/dto/output/error.ts
@@ -1,19 +1,30 @@
 import { z } from "@hono/zod-openapi";
 
-const ErrorSchema = z
-  .object({
-    message: z.string().openapi({ description: "explanation" }),
-  })
-  .openapi("Error");
+const errorCode = [
+  "Bad Request",
+  "Not Found",
+  "Internal Server Error",
+] as const;
 
-export type ErrorResponse = z.infer<typeof ErrorSchema>;
+const errorSchemaFactory = (code: (typeof errorCode)[number]) => {
+  return z
+    .object({
+      code: z.enum(errorCode).openapi({
+        example: code,
+      }),
+      message: z.string().openapi({ description: "explanation" }),
+    })
+    .openapi("Error");
+};
+
+export type ErrorResponse = z.infer<ReturnType<typeof errorSchemaFactory>>;
 
 export const ErrorResponses = {
   400: {
     description: "Bad Request",
     content: {
       "application/json": {
-        schema: ErrorSchema,
+        schema: errorSchemaFactory("Bad Request"),
       },
     },
   },
@@ -21,7 +32,7 @@ export const ErrorResponses = {
     description: "Not Found",
     content: {
       "application/json": {
-        schema: ErrorSchema,
+        schema: errorSchemaFactory("Not Found"),
       },
     },
   },
@@ -29,7 +40,7 @@ export const ErrorResponses = {
     description: "Internal Server Error",
     content: {
       "application/json": {
-        schema: ErrorSchema,
+        schema: errorSchemaFactory("Internal Server Error"),
       },
     },
   },
