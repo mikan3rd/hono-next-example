a08d93717e29c9a3bbcbc333a02be53fe7f9eb22
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-04-29 12:59:20 +0900

    feat: implement server shutdown handling and log server status

--- packages/backend_app/src/server.ts
+++ packages/backend_app/src/server.ts
@@ -1,7 +1,22 @@
 import { app } from "./apps";
 import { env } from "./env";
 
-export default {
+const server = Bun.serve({
   port: env.BACKEND_APP_PORT,
   fetch: app.fetch,
+  development: false,
+});
+
+const shutdown = async () => {
+  console.info("shutdown event");
+
+  await server.stop();
+  console.info("server stopped");
+
+  process.exit(0);
 };
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+console.info(`Server is running on ${server.url.toString()}`);
