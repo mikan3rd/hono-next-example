037785684a425bce3a870d86c0b8441af40e87cb
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-07-22 09:55:44 +0900

    chore(config): add nursery rule for noFloatingPromises and update TypeScript target to ESNext

--- packages/backend_app/src/scripts/export-openapi/index.ts
+++ packages/backend_app/src/scripts/export-openapi/index.ts
@@ -1,20 +1,5 @@
 import { exportOpenAPI } from "./functions";
 
-(async () => {
-  await exportOpenAPI();
+await exportOpenAPI();
 
-  const { success, stdout } = Bun.spawnSync([
-    "bun",
-    "--bun",
-    "biome",
-    "check",
-    "openapi.json",
-    "--write",
-  ]);
-
-  if (success) {
-    console.info(stdout.toString());
-  } else {
-    console.error("biome check failed");
-  }
-})();
+Bun.spawnSync(["biome", "check", "openapi.json", "--write"]);
