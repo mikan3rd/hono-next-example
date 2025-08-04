ad008a0f4dc8a795878d1501a7ac472acbf87124
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-08-04 16:39:09 +0900

    chore(e2e): update Playwright reporter configuration to conditionally open HTML report

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -17,7 +17,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    [
+      "html",
+      {
+        open: env.CI ? "never" : "always",
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
