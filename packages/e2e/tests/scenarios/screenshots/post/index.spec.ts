f2428edd34c7b57b03acf6e6abc17786bdb06290
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-08-23 16:27:13 +0900

    fix(e2e): update backend URL in docker-compose and enhance post page tests

--- packages/e2e/tests/scenarios/screenshots/post/index.spec.ts
+++ packages/e2e/tests/scenarios/screenshots/post/index.spec.ts
@@ -1,10 +1,24 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("post page", () => {
-  test("has title and message", async ({ page }) => {
+  // TODO: データを初期化した状態でテストする
+  test.skip("has title and message", async ({ page }) => {
     await page.goto("/post");
     await expect(page).toHaveTitle(/posts: 0/);
     await expect(page.getByText("No posts yet")).toBeVisible();
     await expect(page).toHaveScreenshot();
+  });
+
+  test("post content is displayed", async ({ page }) => {
+    await page.goto("/post");
+    const postContent = "This is test content";
+    await page
+      .getByPlaceholder("Write your post content here...")
+      .fill(postContent);
+    await page.getByRole("button", { name: "Create Post" }).click();
+    await expect(
+      page.getByRole("button", { name: "Create Post" }),
+    ).toBeDisabled();
+    await expect(page.getByText(postContent)).toBeVisible();
   });
 });
