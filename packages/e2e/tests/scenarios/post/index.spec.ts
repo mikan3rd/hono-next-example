b617700f5ef59e3d98d62cb7dbdeb36e75de820d
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-08-25 23:15:35 +0900

    feat(post): refactor post creation and update steps, add new screenshots

--- packages/e2e/tests/scenarios/post/index.spec.ts
+++ packages/e2e/tests/scenarios/post/index.spec.ts
@@ -25,49 +25,64 @@ test("post page", async ({ page }) => {
     await expect.soft(page).toHaveScreenshot(screenshotOptions);
   });
 
-  const textArea = page.getByPlaceholder("Write your post content here...");
+  const createTextArea = page.getByPlaceholder(
+    "Write your post content here...",
+  );
   const firstPostContent = `This is first post content`;
   await test.step("create posts", async () => {
     await test.step("create first post", async () => {
-      await textArea.fill(firstPostContent);
+      await createTextArea.fill(firstPostContent);
       await page.getByRole("button", { name: "Create Post" }).click();
-      await expect(textArea).toHaveValue("");
+      await expect(createTextArea).toHaveValue("");
       await expect(page.getByText(firstPostContent)).toBeVisible();
       await expect.soft(page).toHaveScreenshot(screenshotOptions);
     });
 
     await test.step("create second post", async () => {
       const secondPostContent = `This is second post content`;
-      await textArea.fill(secondPostContent);
+      await createTextArea.fill(secondPostContent);
       await page.getByRole("button", { name: "Create Post" }).click();
-      await expect(textArea).toHaveValue("");
+      await expect(createTextArea).toHaveValue("");
       await expect(page.getByText(secondPostContent)).toBeVisible();
       await expect.soft(page).toHaveScreenshot(screenshotOptions);
     });
   });
 
   const updatedPostContent = `This is updated post content`;
-  await test.step("update first post", async () => {
-    await page.getByRole("button", { name: "Edit" }).nth(1).click();
-    await page.getByRole("textbox").nth(1).fill(updatedPostContent);
-    await page.getByRole("button", { name: "Save" }).click();
-    await expect(page.getByText(updatedPostContent)).toBeVisible();
-    await expect.soft(page).toHaveScreenshot(screenshotOptions);
-  });
+  await test.step("update post", async () => {
+    const editTextArea = page.getByRole("textbox").nth(1);
 
-  await test.step("cancel update first post", async () => {
-    await page.getByRole("button", { name: "Edit" }).nth(1).click();
-    await page
-      .getByRole("textbox")
-      .nth(1)
-      .fill("This is not updated post content");
-    await page.getByRole("button", { name: "Cancel" }).click();
-    await expect(page.getByText(updatedPostContent)).toBeVisible();
+    await test.step("check first post edit button", async () => {
+      await page.getByRole("button", { name: "Edit" }).nth(1).click();
+      await expect(editTextArea).toBeVisible();
+      await expect.soft(page).toHaveScreenshot(screenshotOptions);
+    });
+
+    await test.step("update first post", async () => {
+      await expect(page.getByText(firstPostContent)).toBeVisible();
+      await expect(page.getByText("New").nth(2)).toBeVisible();
+
+      await editTextArea.fill(updatedPostContent);
+      await page.getByRole("button", { name: "Save" }).click();
+      await expect(page.getByText(updatedPostContent)).toBeVisible();
+
+      await expect(page.getByText(firstPostContent)).not.toBeVisible();
+      await expect(page.getByText("New").nth(2)).not.toBeVisible();
+
+      await expect.soft(page).toHaveScreenshot(screenshotOptions);
+    });
+
+    await test.step("cancel update first post", async () => {
+      await page.getByRole("button", { name: "Edit" }).nth(1).click();
+      await editTextArea.fill("This is not updated post content");
+      await page.getByRole("button", { name: "Cancel" }).click();
+      await expect(page.getByText(updatedPostContent)).toBeVisible();
+    });
   });
 
   await test.step("delete first post", async () => {
     await page.getByRole("button", { name: "Delete" }).nth(1).click();
-    await expect(page.getByText(firstPostContent)).not.toBeVisible();
+    await expect(page.getByText(updatedPostContent)).not.toBeVisible();
     await expect.soft(page).toHaveScreenshot(screenshotOptions);
   });
 });
