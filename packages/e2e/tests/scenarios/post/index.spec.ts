fcae231685fa4b646af3f3fd51da5af521cf983e
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-08-26 10:56:20 +0900

    feat(post): enhance post testing by utilizing postCard data-testid for improved visibility checks

--- packages/e2e/tests/scenarios/post/index.spec.ts
+++ packages/e2e/tests/scenarios/post/index.spec.ts
@@ -30,12 +30,17 @@ test("post page", async ({ page }) => {
   );
   const firstPostContent = `This is first post content`;
   const secondPostContent = `This is second post content`;
+
   await test.step("create posts", async () => {
     await test.step("create first post", async () => {
       await createTextArea.fill(firstPostContent);
       await page.getByRole("button", { name: "Create Post" }).click();
       await expect(createTextArea).toHaveValue("");
-      await expect(page.getByText(firstPostContent)).toBeVisible();
+
+      const postCards = page.getByTestId("postCard");
+      const firstPostCard = postCards.filter({ hasText: firstPostContent });
+      await expect(postCards).toHaveCount(1);
+      await expect(firstPostCard).toBeVisible();
       await expect.soft(page).toHaveScreenshot(screenshotOptions);
     });
 
@@ -43,46 +48,61 @@ test("post page", async ({ page }) => {
       await createTextArea.fill(secondPostContent);
       await page.getByRole("button", { name: "Create Post" }).click();
       await expect(createTextArea).toHaveValue("");
-      await expect(page.getByText(secondPostContent)).toBeVisible();
+
+      const postCards = page.getByTestId("postCard");
+      const secondPostCard = postCards.filter({ hasText: secondPostContent });
+      await expect(secondPostCard).toBeVisible();
       await expect.soft(page).toHaveScreenshot(screenshotOptions);
     });
   });
 
+  const postCards = page.getByTestId("postCard");
+  const firstPostCard = postCards.filter({ hasText: firstPostContent });
+  const secondPostCard = postCards.filter({ hasText: secondPostContent });
+
   const updatedPostContent = `This is updated post content`;
   await test.step("update post", async () => {
-    const editTextArea = page.getByRole("textbox").nth(1);
+    const editTextArea = firstPostCard.getByRole("textbox");
 
     await test.step("check first post edit button", async () => {
-      await page.getByRole("button", { name: "Edit" }).nth(1).click();
+      await firstPostCard.getByRole("button", { name: "Edit" }).click();
       await expect(editTextArea).toBeVisible();
       await expect.soft(page).toHaveScreenshot(screenshotOptions);
     });
 
     await test.step("update first post", async () => {
-      await expect(page.getByText(firstPostContent)).toBeVisible();
-      await expect(page.getByText("New").nth(2)).toBeVisible();
+      await expect(firstPostCard.getByText(firstPostContent)).toBeVisible();
+      await expect(firstPostCard.getByText("New")).toBeVisible();
 
       await editTextArea.fill(updatedPostContent);
       await page.getByRole("button", { name: "Save" }).click();
-      await expect(page.getByText(updatedPostContent)).toBeVisible();
+      const updatedPostCard = postCards.filter({ hasText: updatedPostContent });
+      await expect(updatedPostCard).toBeVisible();
 
-      await expect(page.getByText(firstPostContent)).not.toBeVisible();
-      await expect(page.getByText("New").nth(2)).not.toBeVisible();
+      await expect(firstPostCard.getByText(firstPostContent)).not.toBeVisible();
+      await expect(firstPostCard.getByText("New")).not.toBeVisible();
 
       await expect.soft(page).toHaveScreenshot(screenshotOptions);
     });
 
-    await test.step("cancel update first post", async () => {
-      await page.getByRole("button", { name: "Edit" }).nth(1).click();
-      await editTextArea.fill("This is not updated post content");
-      await page.getByRole("button", { name: "Cancel" }).click();
-      await expect(page.getByText(updatedPostContent)).toBeVisible();
+    await test.step("cancel update second post", async () => {
+      await secondPostCard.getByRole("button", { name: "Edit" }).click();
+      const notUpdatedPostContent = "This is not updated post content";
+      await secondPostCard.getByRole("textbox").fill(notUpdatedPostContent);
+      const updatingPostCard = postCards.filter({
+        hasText: notUpdatedPostContent,
+      });
+      await expect(updatingPostCard).toBeVisible();
+
+      await updatingPostCard.getByRole("button", { name: "Cancel" }).click();
+      await expect(secondPostCard.getByText(secondPostContent)).toBeVisible();
+      await expect(updatingPostCard).not.toBeVisible();
     });
   });
 
   await test.step("delete second post", async () => {
-    await page.getByRole("button", { name: "Delete" }).first().click();
-    await expect(page.getByText(secondPostContent)).not.toBeVisible();
+    await secondPostCard.getByRole("button", { name: "Delete" }).click();
+    await expect(secondPostCard).not.toBeVisible();
     await expect.soft(page).toHaveScreenshot(screenshotOptions);
   });
 });
