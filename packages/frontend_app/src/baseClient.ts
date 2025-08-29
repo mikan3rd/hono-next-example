f99d54ee3540a079ad351cff34622f8fa3ed6392
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-08-30 01:02:27 +0900

    chore: update schema version and adjust environment variables for frontend

--- packages/frontend_app/src/baseClient.ts
+++ packages/frontend_app/src/baseClient.ts
@@ -2,15 +2,4 @@ import type { AppType } from "backend_app/src/apps";
 import { hc } from "hono/client";
 import { env } from "./env";
 
-/**
- * FIXME:
- * When using docker compose, the URLs are different
- * because the server-side request executed in the container
- * and the client-side execution executed from the browser must be considered.
- */
-const baseUrl =
-  typeof window === "undefined"
-    ? env.BACKEND_APP_SERVER_URL
-    : env.NEXT_PUBLIC_BACKEND_APP_URL;
-
-export const baseClient = hc<AppType>(baseUrl);
+export const baseClient = hc<AppType>(env.NEXT_PUBLIC_BACKEND_APP_URL);
--- packages/frontend_app/src/env.ts
+++ packages/frontend_app/src/env.ts
@@ -2,16 +2,11 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    BACKEND_APP_SERVER_URL: z.url(),
-  },
-
   client: {
     NEXT_PUBLIC_BACKEND_APP_URL: z.url(),
   },
 
   runtimeEnv: {
-    BACKEND_APP_SERVER_URL: process.env.BACKEND_APP_SERVER_URL,
     NEXT_PUBLIC_BACKEND_APP_URL: process.env.NEXT_PUBLIC_BACKEND_APP_URL,
   },
 });
