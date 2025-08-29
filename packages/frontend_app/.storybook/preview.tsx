a4e5d0191bc6980da5df10825bbf2f691ee2f168
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-08-29 23:55:06 +0900

    feat(storybook): add preview configuration and PostCard stories

--- packages/frontend_app/.storybook/preview.tsx
+++ packages/frontend_app/.storybook/preview.tsx
@@ -1,5 +1,6 @@
 import type { Preview } from "@storybook/nextjs-vite";
 import "../src/app/globals.css";
+import Providers from "../src/app/providers";
 
 const preview: Preview = {
   parameters: {
@@ -17,6 +18,13 @@ const preview: Preview = {
       test: "todo",
     },
   },
+  decorators: [
+    (Story) => (
+      <Providers >
+        <Story />
+      </Providers>
+    ),
+  ],
 };
 
 export default preview;
