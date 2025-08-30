f8a1aab2184471ba86402b2a63b7582ec7c412ea
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-08-30 22:34:44 +0900

    feat(storybook): enhance PostForm story with user interaction tests
    Added user interaction tests to the PostForm story, verifying the initial state of the input and button, and ensuring the button is enabled after typing in the input field.

--- packages/frontend_app/src/components/pages/post/components/PostForm/index.stories.tsx
+++ packages/frontend_app/src/components/pages/post/components/PostForm/index.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
-import { fn } from "storybook/test";
+import { expect, fn, within } from "storybook/test";
 import { PostForm } from ".";
 
 const meta = {
@@ -13,5 +13,18 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     invalidatePostsQuery: fn(),
+  },
+  play: async ({ canvasElement, userEvent }) => {
+    const canvas = within(canvasElement);
+
+    const input = canvas.getByRole("textbox");
+    expect(input).toHaveValue("");
+    const createPostButton = canvas.getByRole("button", {
+      name: "Create Post",
+    });
+    expect(createPostButton).toBeDisabled();
+
+    await userEvent.type(input, "test");
+    expect(createPostButton).toBeEnabled();
   },
 };
