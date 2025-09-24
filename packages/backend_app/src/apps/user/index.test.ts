7a5397d1a0dbff75924d00ad3bde2abc04bc5e0b
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-09-24 20:13:53 +0900

    feat: add user retrieval test after signup
    Implemented a test to verify that a user is correctly inserted into the database after signing up. This enhances the test coverage for the user signup functionality by checking the presence and properties of the newly created user.

--- packages/backend_app/src/apps/user/index.test.ts
+++ packages/backend_app/src/apps/user/index.test.ts
@@ -2,6 +2,8 @@ import { beforeEach, describe, expect, it } from "bun:test";
 import type { ClientRequestOptions } from "hono/client";
 import { testClient } from "hono/testing";
 import { app } from "../../apps";
+import { db } from "../../db";
+import { usersTable } from "../../db/schema";
 
 describe("userApp", () => {
   describe("signupRoute", () => {
@@ -19,6 +21,13 @@ describe("userApp", () => {
     it("should return 200 Response", async () => {
       const res = await subject();
       expect(res.status).toBe(200);
+
+      const users = await db.select().from(usersTable);
+      expect(users).toHaveLength(1);
+
+      const user = users[0];
+      if (!user) throw new Error("user is not found");
+      expect(user.supabase_uid).toBe("11111111-1111-1111-1111-111111111111");
     });
 
     describe("when Authorization header is not provided", () => {
