2f0272d2badc05deb3a870a8915ef2e0e133a9fb
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-09-30 11:00:02 +0900

    fix(db): add notice handling to postgres client to suppress specific logs

--- packages/backend_app/src/db/index.ts
+++ packages/backend_app/src/db/index.ts
@@ -2,7 +2,16 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { DATABASE_URL } from "../env";
 
-const client = postgres(DATABASE_URL);
+const client = postgres(DATABASE_URL, {
+  onnotice: (notice) => {
+    // To prevent truncateAllTables logs
+    if (notice.code === "00000") {
+      return;
+    }
+    // biome-ignore lint/suspicious/noConsole: ok
+    return console.log(notice);
+  },
+});
 
 export const db = drizzle({
   client,
