1bd0ec81ca5571b864f5932f4dbcb23da2f51f4c
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-10-01 00:04:26 +0900

    refactor(db): simplify field creation logic using Object.fromEntries

--- packages/backend_app/src/db/field.ts
+++ packages/backend_app/src/db/field.ts
@@ -14,14 +14,10 @@ export const postPublicFieldDefs = [
 
 const createFieldsFromDefs = <T extends readonly string[]>(
   fieldDefs: T,
-): Record<T[number], true> => {
-  return fieldDefs.reduce(
-    (acc, field) => {
-      acc[field as T[number]] = true;
-      return acc;
-    },
-    {} as Record<T[number], true>,
-  );
+): { [K in T[number]]: true } => {
+  return Object.fromEntries(fieldDefs.map((field) => [field, true])) as {
+    [K in T[number]]: true;
+  };
 };
 
 export const userPublicFields = createFieldsFromDefs(userPublicFieldDefs);
