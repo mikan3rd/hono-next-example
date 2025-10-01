04dbdf76b80df4c14561a5280efe75a8900cb5e1
mikan3rd <ota.hiroki@moneyforward.co.jp>
2025-10-01 22:49:36 +0900

    refactor(db): update primary key and public_id handling in schema definition

--- packages/backend_app/src/db/schema.ts
+++ packages/backend_app/src/db/schema.ts
@@ -1,12 +1,10 @@
 import { relations } from "drizzle-orm";
 import { integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-// TODO: uuid v7 を検討
-const primaryKeys = {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-};
-
-const publicIdColumn = () => uuid().unique().notNull().defaultRandom();
+const primaryKeys = () => ({
+  id: integer().primaryKey().generatedAlwaysAsIdentity(), // TODO: uuid v7 を検討
+  public_id: uuid().unique().notNull().defaultRandom(), // uuid v4
+});
 
 const timestamps = {
   created_at: timestamp().defaultNow().notNull(),
@@ -17,16 +15,14 @@ const timestamps = {
 };
 
 export const usersTable = pgTable("users", {
-  ...primaryKeys,
-  public_id: publicIdColumn(),
+  ...primaryKeys(),
   supabase_uid: uuid().unique().notNull(),
   display_name: text().notNull(),
   ...timestamps,
 });
 
 export const postsTable = pgTable("posts", {
-  ...primaryKeys,
-  public_id: publicIdColumn(),
+  ...primaryKeys(),
   user_id: integer()
     .notNull()
     .references(() => usersTable.id),
